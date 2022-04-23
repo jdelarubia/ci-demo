711757e5a744c773aa1311617273d5d704b5d64e
jdelarubia <jdelarubia@gmail.com>
2022-04-23 14:03:02 +0200

    linting config options

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,10 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  rules: {
-  },
+  ignorePatterns: ['*.test.js'],
+  rules: {},
 };
