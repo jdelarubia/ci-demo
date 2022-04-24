92cb1da3305dee09ad9b736c1cc8ff4b7a44a880
jdelarubia <jdelarubia@gmail.com>
2022-04-24 22:08:28 +0200

    add 2 more tests

--- attack.test.js
+++ attack.test.js
@@ -1,7 +1,17 @@
 const { attack } = require('./attack');
 
 test('should return damage sentence when called correctly', () => {
-  const result = attack('Troll', 10, false);
+  const result = attack('Troll', 10);
   expect(result).toBe('Troll dealt 10 damage!');
   expect(result).toMatch(/^Troll(.*)10 damage!/);
+});
+
+test('sentence should contain the word "damage"', () => {
+  const result = attack('Troll', 10, true);
+  expect(result).toMatch(/damage/);
+});
+
+test('if critical damage is set to True, damage should double', () => {
+  const result = attack('Troll', 10, true);
+  expect(result).toMatch(/20/);
 });
